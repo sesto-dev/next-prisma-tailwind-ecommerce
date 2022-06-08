9e8f80bfaebe3a44959bba9b4e5a04b9899ddfed
Accretence <Accretence@users.noreply.github.com>
2022-06-09 00:16:17 +0430

    Simple Rename

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import Badged from './components/Badged'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Helmet from './components/Helmet'
-import Wrapper from './components/Wrapper'
-import YouTube from './components/YouTube'
+import Badged from './src/Badged'
+import Banner from './src/Banner'
+import Footer from './src/Footer'
+import Header from './src/Header'
+import Helmet from './src/Helmet'
+import Wrapper from './src/Wrapper'
+import YouTube from './src/YouTube'
 
 export { Badged, Banner, Footer, Header, Helmet, Wrapper, YouTube }
