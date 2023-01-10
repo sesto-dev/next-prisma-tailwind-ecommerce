89f46c02a7ac16eea49ee524ca19b5830939eba2
Accretence <accretence@outlook.com>
2023-01-10 11:49:29 +0330

    Update Wagmi Lib

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,28 +1,22 @@
-import { createClient, defaultChains, configureChains } from 'wagmi'
-
+import { configureChains, createClient } from 'wagmi'
+import { mainnet, optimism } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
+
 import { publicProvider } from 'wagmi/providers/public'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-
-const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY }),
-    publicProvider(),
-])
+const { chains, provider } = configureChains(
+    [mainnet, optimism],
+    [
+        alchemyProvider({
+            apiKey: process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_KEY,
+        }),
+        publicProvider(),
+    ]
+)
 
 export const client = createClient({
     autoConnect: true,
-    connectors: [
-        new MetaMaskConnector({ chains }),
-
-        new WalletConnectConnector({
-            chains,
-            options: {
-                qrcode: true,
-            },
-        }),
-    ],
+    connectors: [new InjectedConnector({ chains })],
     provider,
-    webSocketProvider,
 })
