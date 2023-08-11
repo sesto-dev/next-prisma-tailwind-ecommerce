628c95b27a3f8ae73e8d0002317632cfecc6fa5f
Accretence <Accretence@outlook.com>
2023-08-11 21:16:26 +0330

    Prisma Seed Schema

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,65 +1,63 @@
-
 import Link from 'next/link'
 
-import { siteConfig } from "config/site"
-import { cn } from "lib/utils"
-import { CommandMenu } from "components/composites/command"
-import { Icons } from "components/icons"
-import { MainNav } from "components/native/nav/main"
-import { MobileNav } from "components/native/nav/mobile"
-import { buttonVariants } from "components/ui/button"
-import { ModeToggle } from "components/composites/mode-toggle"
-
+import { siteConfig } from 'config/site'
+import { cn } from 'lib/utils'
+import { CommandMenu } from 'components/composites/command'
+import { Icons } from 'components/icons'
+import { MainNav } from 'components/native/nav/main'
+import { MobileNav } from 'components/native/nav/mobile'
+import { buttonVariants } from 'components/ui/button'
+import { ModeToggle } from 'components/composites/mode-toggle'
 
 export default function Header() {
-        return (
-            <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
-                <div className="container flex h-14 items-center">
-        <MainNav />
-        <MobileNav />
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            <CommandMenu />
-          </div>
-          <nav className="flex items-center">
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.gitHub className="h-4 w-4" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={cn(
-                  buttonVariants({
-                    variant: "ghost",
-                  }),
-                  "w-9 px-0"
-                )}
-              >
-                <Icons.twitter className="h-4 w-4 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <ModeToggle />
-          </nav>
-        </div>
-      </div>
-            </header>
-        )
-    }
+    return (
+        <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur">
+            <div className="flex h-14 items-center">
+                <MainNav />
+                <MobileNav />
+                <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+                    <div className="w-full flex-1 md:w-auto md:flex-none">
+                        <CommandMenu />
+                    </div>
+                    <nav className="flex items-center">
+                        <Link
+                            href={siteConfig.links.github}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <div
+                                className={cn(
+                                    buttonVariants({
+                                        variant: 'ghost',
+                                    }),
+                                    'w-9 px-0'
+                                )}
+                            >
+                                <Icons.gitHub className="h-4 w-4" />
+                                <span className="sr-only">GitHub</span>
+                            </div>
+                        </Link>
+                        <Link
+                            href={siteConfig.links.twitter}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <div
+                                className={cn(
+                                    buttonVariants({
+                                        variant: 'ghost',
+                                    }),
+                                    'w-9 px-0'
+                                )}
+                            >
+                                <Icons.twitter className="h-4 w-4 fill-current" />
+                                <span className="sr-only">Twitter</span>
+                            </div>
+                        </Link>
+                        <ModeToggle />
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,60 +1,74 @@
-import prisma from '../lib/prisma'
-import { users, blogPosts, discounts, products, docs } from './seed.data'
-import { slugify } from '../lib/slug'
+import { faker } from '@faker-js/faker'
+
+function getRandomIntInRange(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function main() {
-    for (let user of users) {
-        const { email, referralCode, isAdmin, discordId } = user
+    const categories = [
+        'Electronics',
+        'Clothing',
+        'Books',
+        'Home & Kitchen',
+        'Toys',
+    ]
 
+    for (const categoryName of categories) {
+        await prisma.category.create({
+            data: {
+                name: categoryName,
+            },
+        })
+    }
+
+    for (let i = 0; i < 10; i++) {
+        const product = await prisma.product.create({
+            data: {
+                title: faker.commerce.product(),
+                description: faker.commerce.productDescription(),
+                imageUrl: faker.image.url(),
+                categories: {
+                    connect: {
+                        name: categories[
+                            getRandomIntInRange(0, categories.length - 1)
+                        ],
+                    },
+                },
+                variants: {
+                    create: {
+                        title: faker.commerce.productName(),
+                        description: faker.commerce.productDescription(),
+                        price: getRandomIntInRange(0, 100),
+                        imageUrl: faker.image.url(),
+                        stock: getRandomIntInRange(2, 10),
+                        reserved: getRandomIntInRange(0, 1),
+                    },
+                },
+            },
+            include: {
+                categories: true,
+                variants: true,
+            },
+        })
+
+        console.log({ product })
+    }
+
+    console.log('Created Products...')
+
+    for (let i = 0; i < 4; i++) {
         await prisma.user.create({
             data: {
-                email,
-                referralCode,
-                discordId: discordId ? discordId : null,
-                isAdmin,
+                email: faker.internet.email(),
+                name: faker.person.fullName(),
             },
         })
     }
 
     console.log('Created Users...')
-
-    const { id: authorId } = await prisma.user.findFirst()
-
-    for (let blogPost of blogPosts) {
-        const { title, description, image, categories, content } = blogPost
-
-        await prisma.blogPost.create({
-            data: {
-                authorId,
-                title,
-                description,
-                slug: slugify(title),
-                image,
-                content,
-                categories,
-            },
-        })
-    }
-
-    console.log('Created Blog Posts...')
-
-    for (let doc of docs) {
-        const { title, index, category, categoryIndex, content } = doc
-
-        await prisma.documentPage.create({
-            data: {
-                authorId,
-                title,
-                index,
-                category,
-                categoryIndex,
-                slug: slugify(title),
-                content,
-            },
-        })
-    }
-
-    console.log('Created Documentation Pages...')
 }
 
 try {
