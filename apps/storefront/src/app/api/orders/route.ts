656a2adc5ed12a5efc0993041897702cb92babe4
Accretence <Accretence@outlook.com>
2023-09-11 01:25:22 +0330

    Order Creation Endpoint

--- apps/storefront/src/app/api/orders/route.ts
+++ apps/storefront/src/app/api/orders/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server'
+import { sendMail } from '@persepolis/mail'
+import { render } from '@react-email/render'
+import Mail from '@/mails/order_notification_owner'
 
 import prisma from '@/lib/prisma'
+import config from '@/config/site'
 
 export async function GET(req: Request) {
    try {
@@ -13,6 +17,12 @@ export async function GET(req: Request) {
       const orders = await prisma.order.findMany({
          where: {
             userId,
+         },
+         include: {
+            address: true,
+            payments: true,
+            refund: true,
+            orderItems: true,
          },
       })
 
@@ -31,7 +41,18 @@ export async function POST(req: Request) {
          return new NextResponse('Unauthorized', { status: 401 })
       }
 
-      const { addressId } = await req.json()
+      const { addressId, discountCode } = await req.json()
+
+      if (discountCode) {
+         await prisma.discountCode.findUniqueOrThrow({
+            where: {
+               code: discountCode,
+               stock: {
+                  gte: 1,
+               },
+            },
+         })
+      }
 
       const cart = await prisma.cart.findUniqueOrThrow({
          where: {
@@ -46,19 +67,88 @@ export async function POST(req: Request) {
          },
       })
 
+      const { tax, total, discount, payable } = calculateCosts({ cart })
+
       const order = await prisma.order.create({
          data: {
-            userId,
+            user: {
+               connect: {
+                  id: userId,
+               },
+            },
             status: 'Processing',
-            payable: 10,
-            discount: 0,
-            shipping: 5,
+            total,
+            tax,
+            payable,
+            discount,
+            shipping: 0,
+            address: {
+               connect: { id: addressId },
+            },
+            orderItems: {
+               create: cart?.items.map((orderItem) => ({
+                  count: orderItem.count,
+                  price: orderItem.product.price,
+                  discount: orderItem.product.discount,
+                  product: {
+                     connect: {
+                        id: orderItem.productId,
+                     },
+                  },
+               })),
+            },
          },
       })
 
+      const owners = await prisma.owner.findMany()
+
+      const notifications = await prisma.notification.createMany({
+         data: owners.map((owner) => ({
+            userId: owner.id,
+            content: `Order #${order.number} was created was created with a value of $${payable}.`,
+         })),
+      })
+
+      for (const owner of owners) {
+         await sendMail({
+            name: config.name,
+            to: owner.email,
+            subject: 'An order was created.',
+            html: render(
+               Mail({
+                  id: order.id,
+                  payable: payable.toFixed(2),
+                  orderNum: order.number.toString(),
+               })
+            ),
+         })
+      }
+
       return NextResponse.json(order)
    } catch (error) {
-      console.error('[ORDERS_POST]', error)
+      console.error('[ORDER_POST]', error)
       return new NextResponse('Internal error', { status: 500 })
+   }
+}
+
+function calculateCosts({ cart }) {
+   let total = 0,
+      discount = 0
+
+   for (const item of cart?.items) {
+      total += item?.count * item?.product?.price
+      discount += item?.count * item?.product?.discount
+   }
+
+   const afterDiscount = total - discount
+   const tax = afterDiscount * 0.09
+   const payable = afterDiscount + tax
+
+   return {
+      total: parseFloat(total.toFixed(2)),
+      discount: parseFloat(discount.toFixed(2)),
+      afterDiscount: parseFloat(afterDiscount.toFixed(2)),
+      tax: parseFloat(tax.toFixed(2)),
+      payable: parseFloat(payable.toFixed(2)),
    }
 }
---
+++ apps/storefront/src/mails/order_notification_owner.tsx
@@ -0,0 +1,96 @@
+import {
+   Body,
+   Button,
+   Container,
+   Column,
+   Head,
+   Heading,
+   Hr,
+   Html,
+   Img,
+   Link,
+   Preview,
+   Row,
+   Section,
+   Tailwind,
+   Text,
+} from '@react-email/components'
+import * as React from 'react'
+
+interface VercelInviteUserEmailProps {
+   orderNum?: string
+   payable?: string
+   id?: string
+}
+
+const baseUrl = process.env.VERCEL_URL
+   ? `https://${process.env.VERCEL_URL}`
+   : ''
+
+export const VercelInviteUserEmail = ({
+   orderNum = '0',
+   payable = '0',
+   id = 'My Project',
+}: VercelInviteUserEmailProps) => {
+   const previewText = `Order #${orderNum} was created was created with a value of $${payable}.`
+   const orderLink = process.env.NEXT_PUBLIC_URL + `/orders/${id}`
+   return (
+      <Html>
+         <Head />
+         <Preview>{previewText}</Preview>
+         <Tailwind>
+            <Body className="bg-white my-auto mx-auto font-sans">
+               <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] w-[465px]">
+                  <Section className="mt-[32px]">
+                     <Img
+                        src={`${baseUrl}/static/vercel-logo.png`}
+                        width="40"
+                        height="37"
+                        alt="Vercel"
+                        className="my-0 mx-auto"
+                     />
+                  </Section>
+                  <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+                     Check the order.
+                  </Heading>
+                  <Text className="text-black text-[14px] leading-[24px]">
+                     Hello!
+                  </Text>
+                  <Text className="text-black text-[14px] leading-[24px]">
+                     Order #{orderNum} was created was created with a value of $
+                     {payable}.
+                  </Text>
+
+                  <Section className="text-center mt-[32px] mb-[32px]">
+                     <Button
+                        pX={20}
+                        pY={12}
+                        className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center"
+                        href={orderLink}
+                     >
+                        See the order.
+                     </Button>
+                  </Section>
+                  <Text className="text-black text-[14px] leading-[24px]">
+                     or copy and paste this URL into your browser:{' '}
+                     <Link
+                        href={orderLink}
+                        className="text-blue-600 no-underline"
+                     >
+                        {orderLink}
+                     </Link>
+                  </Text>
+                  <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+                  <Text className="text-[#666666] text-[12px] leading-[24px]">
+                     If you were not expecting this invitation, you can ignore
+                     this email. If you are concerned about your account's
+                     safety, please reply to this email to get in touch with us.
+                  </Text>
+               </Container>
+            </Body>
+         </Tailwind>
+      </Html>
+   )
+}
+
+export default VercelInviteUserEmail
